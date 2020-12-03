d698f13038f7074247259f164c0b07449b5a9d09
Luca <qrtlcu96m28d086k@studenti.unical.it>
2020-12-03 17:54:27 +0100

    patata

--- Binairo/src/gui/GameManager.java
+++ Binairo/src/gui/GameManager.java
@@ -1,5 +1,7 @@
 package gui;
 
+import java.util.ArrayList;
+import java.util.Iterator;
 import application.Main;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -8,6 +10,9 @@ import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.GridPane;
+import javafx.scene.shape.Circle;
 
 public class GameManager {
 	
@@ -20,13 +25,17 @@ public class GameManager {
     private Button start;
 	@FXML
     private Button exit;
-    
-	ObservableList<Integer> dim = FXCollections.observableArrayList(6,8,10,14,20);
-	private int matrix_size;
+    @FXML
+    private GridPane gridPane = new GridPane();
+	private int matrix_size=6;
+	    
+    private Button[][] circles;
+	private Boolean[][] matrix;
 	
-	private Boolean[][] matrix = new Boolean[6][6];
+	ObservableList<Integer> dim = FXCollections.observableArrayList(6,8,10,14,20);
 	
     public GameManager() {}
+    
 
 	@FXML
     void initialize() {
@@ -35,14 +44,89 @@ public class GameManager {
 		size.setValue(6);
 		
 		start.setOnMouseClicked(new EventHandler<Event>() {
+			
+			int problem_size = 0;		
+					
 			@Override
     		public void handle(Event event) {
     			matrix_size = (int) size.getValue();
+    			
+    			switch (matrix_size) {
+    			case 6:
+    				problem_size = 1;
+    				break;
+    			case 8:
+    				problem_size = 3;
+    				break;
+    			case 10:
+    				problem_size = 5;
+    				break;
+    			case 14:
+    				problem_size = 7;
+    				break;
+    			case 20:
+    				problem_size = 9;
+    				break;
+
+    			default:
+    				break;
+    			}
+    			
+    			circles = new Button[matrix_size][matrix_size]; 
+    			matrix = new Boolean[matrix_size][matrix_size];
+    			
     			System.out.println(matrix_size);
     			
-    			initMatrix("c0c1g0f1c0a110e");
+    			initMatrix(getString(problem_size));
+    			
+    			for (int i = 0; i < matrix_size; i++) {
+					for (int j = 0; j < matrix_size; j++) {
+		    			final Integer innerI = new Integer(i);
+		    			final Integer innerJ = new Integer(j);
+						circles[i][j] = new Button();
+						circles[i][j].setStyle("-fx-background-color: #b0b0b0");
+						circles[i][j].setMinWidth(gridPane.getWidth()/matrix_size);
+						circles[i][j].setMinHeight(gridPane.getHeight()/matrix_size);
+		    			circles[i][j].setOnMouseClicked(new EventHandler<Event>() {
+				    		@Override
+				    		public void handle(Event event) {
+				    			if(matrix[innerI][innerJ]==null){
+				    				circles[innerI][innerJ].setStyle("-fx-background-color: #000000");	
+				    				matrix[innerI][innerJ]=true;
+				    			}
+				    			else if(matrix[innerI][innerJ]){
+				    				circles[innerI][innerJ].setStyle("-fx-background-color: #ffffff");	
+				    				matrix[innerI][innerJ]=false;				    			
+				    			}
+				    			else if(!matrix[innerI][innerJ]){
+				    				circles[innerI][innerJ].setStyle("-fx-background-color: #b0b0b0");	
+				    				matrix[innerI][innerJ]=null;				    			
+				    			}		    			
+				    		}
+				    	});
+					}
+				}
+    			
+    			for (int i = 0; i < matrix_size; i++) {
+					for (int j = 0; j < matrix_size; j++) {
+						gridPane.add(circles[i][j], i, j, 1, 1);
+					}
+				}
+    			
+    			for (int i = 0; i < matrix_size; i++) {
+					for (int j = 0; j < matrix_size; j++) {
+						if(matrix[i][j] != null) {
+							if(!matrix[i][j])
+								circles[j][i].setStyle("-fx-background-color: #ffffff");
+							else
+								circles[j][i].setStyle("-fx-background-color: #000000");
+						}
+					}
+				}
+    			
     			printMatrix();
     		}
+			
     	});
 		
     	exit.setOnMouseClicked(new EventHandler<Event>() {
@@ -62,13 +146,16 @@ public class GameManager {
 		this.main = main;
 	}
 	
+	public String getString(int problem_size) {
+		
+		return null;
+	}
+	
 	
 	public void initMatrix(String s) {
 		
 		int i=0,j=0;
-		for(char c : s.toCharArray()) {
-//			System.out.println(c + " " + (c- 'a'+1));
-			
+		for(char c : s.toCharArray()) {			
 			Integer value = null;
 			try {
 				value = Integer.parseInt(""+c);
@@ -87,7 +174,6 @@ public class GameManager {
 					i++;
 				}
 			}
-			
 		}
 	}
 	
