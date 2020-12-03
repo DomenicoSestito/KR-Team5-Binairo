5bc38494627a3c5fc56220f977fb29d97ff2d5b2
Thomas Voce <thom_96@hotmail.it>
2020-12-03 16:44:46 +0100

    parser code init

--- Binairo/src/gui/GameManager.java
+++ Binairo/src/gui/GameManager.java
@@ -24,6 +24,8 @@ public class GameManager {
 	ObservableList<Integer> dim = FXCollections.observableArrayList(6,8,10,14,20);
 	private int matrix_size;
 	
+	private Boolean[][] matrix = new Boolean[6][6];
+	
     public GameManager() {}
 
 	@FXML
@@ -38,6 +40,8 @@ public class GameManager {
     			matrix_size = (int) size.getValue();
     			System.out.println(matrix_size);
     			
+    			initMatrix("c0c1g0f1c0a110e");
+    			printMatrix();
     		}
     	});
 		
@@ -56,6 +60,45 @@ public class GameManager {
 
 	public void setMain(Main main) {
 		this.main = main;
+	}
+	
+	
+	public void initMatrix(String s) {
+		
+		int i=0,j=0;
+		for(char c : s.toCharArray()) {
+//			System.out.println(c + " " + (c- 'a'+1));
+			
+			Integer value = null;
+			try {
+				value = Integer.parseInt(""+c);
+			}catch(Exception e) {}
+			
+			int skip = 1;
+			if(value != null)
+				matrix[i][j] = value == 1;
+			else 
+				skip = (c - 'a'+1);
+			
+			for(int k=0;k<skip;++k) {
+				j++;
+				if(j==matrix_size) {
+					j=0;
+					i++;
+				}
+			}
+			
+		}
+	}
+	
+	
+	public void printMatrix() {
+		for(int i=0;i<matrix_size;++i) {
+			for (int j = 0; j < matrix_size; j++) {
+				System.out.print(matrix[i][j]);
+			}
+			System.out.println();
+		}
 	}
 
 }
